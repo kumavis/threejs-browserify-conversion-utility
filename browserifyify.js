5cb3727259de48c58c986ebf681306447b9ff1d8
Colin Ballast <coballast@gmail.com>
2015-03-01 08:57:16 -1000

    Refactored functions and chose more sensible (and verbose) names

--- browserifyify.js
+++ browserifyify.js
@@ -39,52 +39,51 @@ var addCorrespondingFile = function(file){
   }
 };
 
-var processFilesRecursive = function(file, dependencies){
-  addCorrespondingFile(file);
+// Calculate the dependencies and abstract syntax trees of a particular javascript file
+var calcJSDependenciesAndASTs = function(file, dependencies, asts){
+
+  // if it is not a javascript file, return without processing
+  if(path.extname(file) != '.js')
+    return;
+
+  dependencies[file] = {
+    definedObjects: [],
+    usedObjects: []
+  };
+
+  var tree = esprima.parse(fs.readFileSync(file));
+  asts[file] = tree; // save tree for future use
+  estraverse.traverse(tree, {
+    enter: function(node, parent){
+      if(isThreeAssignment(node)){
+        dependencies[file].definedObjects.push(
+           node.left.property.name
+        );
+        dependencies[file].definedObjects = dependencies[file].definedObjects.getUnique();
+      } else if(isThreeObject(node)){
+          dependencies[file].usedObjects.push(
+            node.property.name
+          );
+          dependencies[file].usedObjects = dependencies[file].usedObjects.getUnique();
+        }
+      }
+    }
+  );
+};
+
+var calculateDependenciesAndASTs = function(file, dependencies, asts){
   if(fs.lstatSync(file).isDirectory()){
+    // If what we have is a directory, then we want to go through
+    // each file in the directory and calculate the dependencies of each one.
     var file_list = fs.readdirSync(file);
     for(var i = 0; i < file_list.length; i++){
-      processFilesRecursive(path.join(file, file_list[i]), dependencies);
+      calculateDependenciesAndASTs(path.join(file, file_list[i]), dependencies, asts);
     }
-  }else if(path.extname(file)=='.js'){
-    dependencies[file] = {
-      definedObjects: [],
-      usedObjects: []
-    };
-
-    var tree = esprima.parse(fs.readFileSync(file));
-    estraverse.traverse(tree, {
-
-      enter: function(node, parent){
-        if(isThreeAssignment(node)){
-          dependencies[file].definedObjects.push(
-             node.left.property.name
-          );
-          dependencies[file].definedObjects = dependencies[file].definedObjects.getUnique();
-        } else if(isThreeObject(node)){
-            dependencies[file].usedObjects.push(
-              node.property.name
-            );
-            dependencies[file].usedObjects = dependencies[file].usedObjects.getUnique();
-          }
-        }
-      });
-  }
-}
+  }else calcJSDependenciesAndASTs(file, dependencies, asts);
+};
 
 if(process.argv.length < 2)
   throw new Error("You must supply a path for the three.js directory.");
-
-
-var dependencies = {};
-// An object that will hold the following information:
-// The path & name of each file
-// What library specific objects/constants (THREE.** stuff) are defined in what file
-// What constants/objects are mentioned in the file, which can also include defined things
-
-// This structure will be used to calculate/infer dependencies and determine
-// the require statements and exports statements to insert into each file.
-
 
 var three_path = path.normalize(process.argv[2]);
 // self explanatory (I hope)
@@ -93,8 +92,23 @@ var src_path = path.join(three_path, 'src');
 // the path we will read source files from
 
 var browserify_src = path.join(__dirname, 'src');
+if(fs.lstatSync(browserify_src).isDirectory())
+  fs.rmdir(browserify_src);
 fs.mkdirSync(browserify_src);
 // the path we will write the new browserify compatible source files to
 
-processFilesRecursive(src_path, dependencies);
+var dependencies = {},
+// An object that will hold the following information:
+// The path & name of each file
+// What library specific objects/constants (THREE.** stuff) are defined in what file
+// What constants/objects are mentioned in the file, which can also include defined things
+
+// This structure will be used to calculate/infer dependencies and determine
+// the require statements and exports statements to insert into each file.
+
+asts = {};
+// An object that holds the abstract syntax tree of each javascript file encountered.
+// Used as a cache to avoid unecessary processing and file reads in the future.
+calculateDependenciesAndASTs(src_path, dependencies, asts);
+// calculate initial dependencies, first pass
 console.log(dependencies)
