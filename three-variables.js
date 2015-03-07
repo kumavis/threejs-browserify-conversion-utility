50c417f3d92e047756fc196e08eaec1a11dd9803
Colin Ballast <coballast@gmail.com>
2015-03-06 16:45:48 -1000

    Added helpful comment.

--- three-variables.js
+++ three-variables.js
@@ -1,8 +1,13 @@
+// Everything that has to do with simple find and replace of
+// variable names in files goes here.
+
+
 var estraverse = require('estraverse');
 var threeUtils = require('./three-utils');
 var isThreeObject = threeUtils.isThreeObject;
 var isGlobalThreeObject = threeUtils.isGlobalThreeObject;
 var isNonGlobalAssignmentExpression = threeUtils.isNonGlobalAssignmentExpression;
+
 // If an object/constant/whatever was not declared in the top level Three.js file,
 // then we want to remove the leading THREE object from it, since it will
 // ultimately be declared as a local variable and then exported and loaded from
@@ -21,6 +26,13 @@ var replaceNonGlobalThreeObjects = function(dependencies, asts){
 };
 
 // Assumes the node that is passed in is an expression statement
+
+// If we have something of the form threeObject = foobar
+// then we want to stick a var in front of it since it
+// is no longer a global object. That is,
+// if we have something like "THREE.threeObject = foobar",
+// we want it to become "var threeObject = foobar"
+// This function takes care of the var part of the transformation
 var replaceWithVariableDeclaration = function(node){
   var new_node = {};
   new_node.type = "VariableDeclaration";
