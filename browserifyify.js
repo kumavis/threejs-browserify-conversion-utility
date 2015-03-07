de4d03828a021c2a875b19ed410fbd59d1254fbf
Colin Ballast <coballast@gmail.com>
2015-03-06 15:40:47 -1000

    Now partially handles THREE objects in root.

--- browserifyify.js
+++ browserifyify.js
@@ -16,6 +16,9 @@ Array.prototype.getUnique = function(){
    return a;
 }
 
+// Check if a given node is part of the THREE.js library
+// i.e. does the given node represent an expression
+// like "THREE.foo" ?
 var isThreeObject = function(node){
   if(node.type == "MemberExpression" && node.object.name=="THREE")
     return true;
@@ -23,6 +26,10 @@ var isThreeObject = function(node){
     return false;
 };
 
+// Check if a given node is an expression assigning
+// a property of THREE to some value.
+// i.e. does the given node describe an expression
+// of the form "THREE.foo = bar" ?
 var isThreeAssignment = function(node){
   if(node.type == "AssignmentExpression"){
     if(isThreeObject(node.left))
@@ -33,6 +40,8 @@ var isThreeAssignment = function(node){
 
 // Calculate the dependencies and abstract syntax trees of a particular javascript file
 var calcJSDependenciesAndASTs = function(file, dependencies, asts){
+  // Silently do nothing and return if it isn't javascript
+  // Needed for glsl shaders and possibly some other stuff
   if(path.extname(file) != '.js')
     return;
 
@@ -61,6 +70,10 @@ var calcJSDependenciesAndASTs = function(file, dependencies, asts){
   );
 };
 
+// Take a root directory with javascript files in it (src/ in this case)
+// a dependency structure, and an ast structure,
+// and recursively traverse the directory populating the
+// two structures with ast and dependency information
 var calculateDependenciesAndASTs = function(file, dependencies, asts){
   if(fs.lstatSync(file).isDirectory()){
 
@@ -89,6 +102,9 @@ var writeFiles = function(file, dependencies, asts){
     writeJSFile(file, dependencies, asts);
 };
 
+// Check if an object was defined in the top level
+// Three.js file. Such objects need to be treated
+// with special priority.
 var isGlobalThreeObject = function(node, dependencies){
   var three;
   for(var k in dependencies){
@@ -102,6 +118,8 @@ var isGlobalThreeObject = function(node, dependencies){
   return false;
 };
 
+// Check if an object was not defined in the top level Three.js
+// file AND is in fact part of the library.
 var isNonGlobalThreeObject = function(node, dependencies){
   if(isGlobalThreeObject(node, dependencies))
     return false;
@@ -114,6 +132,8 @@ var isNonGlobalThreeObject = function(node, dependencies){
   return false;
 };
 
+// Should be self explanatory at this point.
+// I need this for some reason, I can't remember what for
 var isNonGlobalAssignmentExpression = function(node, dependencies){
   if(node.type == "ExpressionStatement"
   && node.expression.type == "AssignmentExpression"
@@ -170,7 +190,13 @@ var changeNonGlobalsToLocals = function(dependencies, asts){
   }
 };
 
-var getUndefinedUsedObjects = function(dependencies){
+// Given a dependency entry for a particular file of the form
+// {definedObjects: ..., usedObjects: ...}
+// locate and return objects that are used in the file, but
+// are not also declared in the file. In other word,
+// what things that are part of the library and need
+// to be included are used in this file?
+var getUndeclaredUsedObjects = function(dependencies){
   var result = [];
   var definedObjects = dependencies.definedObjects;
   var usedObjects = dependencies.usedObjects;
@@ -190,16 +216,20 @@ var getFileFromObject = function(object, dependencies){
   return null;
 };
 
+// Get a list of files that need to be
+// required by a particular file
 var gatherRequiredFiles = function(file, dependencies){
-  var undefinedUsedObjects = getUndefinedUsedObjects(dependencies[file]);
-  console.log(undefinedUsedObjects);
-  var required_files = undefinedUsedObjects.map(function(value){
+  var undeclaredUsedObjects = getUndeclaredUsedObjects(dependencies[file]);
+  console.log(undeclaredUsedObjects);
+  var required_files = undeclaredUsedObjects.map(function(value){
     return getFileFromObject(value, dependencies)
   }).getUnique();
   console.log(required_files);
   return required_files;
 };
 
+// Get a list of variables that can be exported from a
+// particular file
 var getRequiredVariables = function(file, dependencies){
   if(path.basename(file) == "Three.js")
     return ["THREE"];
@@ -209,6 +239,9 @@ var getRequiredVariables = function(file, dependencies){
     return [];
 };
 
+// Take a path, and a list of variables that need
+// to be included from the exports object and generate
+// ast node(s) for a require statement from it.
 var generateRequires = function(path, required_variables){
   if(required_variables.length == 1){
     var new_node = {};
@@ -280,28 +313,55 @@ var generateRequires = function(path, required_variables){
   }else
     return [];
 };
+
+// Given our source file, a list of files to require, and
+// a dependency structure, generate relative require statements
+var generateRequireNodes = function(file, file_list, dependencies){
+  var require_paths = file_list.map(function(value){
+    if(value != null)
+      return path.relative(file, value);
+  });
+
+  var required_variables = file_list.map(function(value){
+    return getRequiredVariables(value, dependencies);
+  });
+
+  var require_nodes = [];
+
+  for(var i = 0; i < file_list.length; i++){
+    require_nodes = require_nodes.concat(generateRequires(require_paths[i], required_variables[i]));
+  }
+
+  return require_nodes;
+}
+
 // Take dependencies, calculate requires, then prepend them
 var prependRequires = function(dependencies, asts){
   for(var file in asts){
     if(path.basename(file) != "Three.js"){
       var required_files = gatherRequiredFiles(file, dependencies);
-      var require_paths = required_files.map(function(value){
-        if(value != null)
-          return path.relative(file, value);
-      });
-
-      var required_variables = required_files.map(function(value){
-        return getRequiredVariables(value, dependencies);
-      });
-
-      var require_nodes = [];
-
-      for(var i = 0; i < required_files.length; i++){
-        console.log(required_variables[i]);
-        require_nodes = require_nodes.concat(generateRequires(require_paths[i], required_variables[i]));
-      }
+      var require_nodes = generateRequireNodes(file, required_files, dependencies);
 
       asts[file].body = require_nodes.concat(asts[file].body);
+    } else{
+      // The root Three.js file needs to be handled specially, we
+      // will APPEND require statements at the end, and require
+      // every declared variable available in the library.
+      // We need to append them at the bottom due to the fact
+      // that some objects we require into THREE depend on things defined in
+      // THREE itself, which is a cyclic dependency.
+      // Common.js has a special mechanism to resolve this issue
+      // that we are taking advantage of.
+      // See https://nodejs.org/api/modules.html#modules_cycles
+
+      var required_files = [];
+      for(var i in asts){
+        if(path.basename(i) != "Three.js")
+          required_files.push(i);
+      }
+
+      var require_nodes = generateRequireNodes(file, required_files, dependencies);
+      asts[file].body = asts[file].body.concat(require_nodes);
     }
   }
 };
